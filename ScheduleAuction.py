9ac2236e45c1743d77e2e1049a6d90157c8b5115
Conor Hayes <ch1987@live.ie>
2015-01-28 22:30:15 +0000

    Manually created some sample auction items in the MongoDB database, auctions items have a unique ID and also a start time, loaded these items into application

--- ScheduleAuction.py
+++ ScheduleAuction.py
@@ -15,10 +15,16 @@ class AuctionScheduler:
         publisher = context.socket(zmq.PUB)
         publisher.bind('tcp://127.0.0.1:1000')
 
+    @staticmethod
+    def get_auction_items(database):
+        print('Retrieving auction items...')
+        return database.auctions.find()
+
 
 if __name__ == '__main__':
     auctionScheduler = AuctionScheduler()
     client = MongoClient()
     db = client.AuctionData
-    print(db)
+    jobs = auctionScheduler.get_auction_items(db)
+    print(jobs)
     auctionScheduler.initialize_publisher()
