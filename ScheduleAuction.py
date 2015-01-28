38177c94299b69bcc84d3c6f6a3b622861a48fab
Conor Hayes <ch1987@live.ie>
2015-01-28 22:27:36 +0000

    Make a connection to 0mq from the application

--- ScheduleAuction.py
+++ ScheduleAuction.py
@@ -1,8 +1,24 @@
 __author__ = 'Conor'
 
 from pymongo import MongoClient
+import zmq
+
+publisher = None
+context = zmq.Context()
+
+
+class AuctionScheduler:
+
+    @staticmethod
+    def initialize_publisher():
+        global publisher
+        publisher = context.socket(zmq.PUB)
+        publisher.bind('tcp://127.0.0.1:1000')
+
 
 if __name__ == '__main__':
+    auctionScheduler = AuctionScheduler()
     client = MongoClient()
     db = client.AuctionData
     print(db)
+    auctionScheduler.initialize_publisher()
