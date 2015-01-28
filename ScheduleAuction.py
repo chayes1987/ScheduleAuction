c9028cbb112c9ec015942a34ef0fddba3f1fe99a
Conor Hayes <ch1987@live.ie>
2015-01-28 22:33:49 +0000

    Scheduled loaded auction items using APScheduler library

--- ScheduleAuction.py
+++ ScheduleAuction.py
@@ -1,6 +1,8 @@
 __author__ = 'Conor'
 
 from pymongo import MongoClient
+from apscheduler.schedulers.background import BlockingScheduler
+from datetime import datetime
 import zmq
 
 publisher = None
@@ -20,11 +22,29 @@ class AuctionScheduler:
         print('Retrieving auction items...')
         return database.auctions.find()
 
+    def schedule_jobs(self, sched, auction_items):
+        for item in auction_items:
+            start_time = datetime.strptime(item['start_time'], '%d-%m-%Y %H:%M:%S')
+            if start_time > datetime.now():
+                item_id = item['_id']
+                sched.add_job(self.publish_start_auction_command, 'date', run_date=start_time, args=[item_id])
+
+    def initialize_scheduler(self, db_jobs):
+        scheduler = BlockingScheduler()
+        self.schedule_jobs(scheduler, db_jobs)
+        print('Scheduler Running...')
+
+        try:
+            scheduler.start()
+        except (KeyboardInterrupt, SystemExit):
+            print('Scheduler Stopped...')
+            pass
+
 
 if __name__ == '__main__':
     auctionScheduler = AuctionScheduler()
     client = MongoClient()
     db = client.AuctionData
     jobs = auctionScheduler.get_auction_items(db)
-    print(jobs)
     auctionScheduler.initialize_publisher()
+    auctionScheduler.initialize_scheduler(jobs)
