8ad02d3629f52521e75e7f4b149a88416748b1cc
Conor Hayes <ch1987@live.ie>
2015-03-19 18:32:40 +0000

    Refactoring, naming, tidying up

--- main.py
+++ main.py
@@ -24,13 +24,13 @@ def read_config():
     conf = ConfigParser()
     try:
         conf.read_file(open('config.ini'))
-        ack_address = conf.get('Addresses', 'ACK_ADDR')
         pub_address = conf.get('Addresses', 'PUB_ADDR')
-        sub_address = conf.get('Addresses', 'SUB_ADDR')
+        ack_address = conf.get('Addresses', 'ACK_ADDR')
         topic = conf.get('Topics', 'START_AUCTION_TOPIC')
         ack_topic = conf.get('Topics', 'START_AUCTION_ACK_TOPIC')
         heartbeat_topic = conf.get('Topics', 'CHECK_HEARTBEAT_TOPIC')
         response_topic = conf.get('Topics', 'CHECK_HEARTBEAT_TOPIC_RESPONSE')
+        sub_address = conf.get('Addresses', 'SUB_ADDR')
         service_name = conf.get('Service Name', 'SERVICE_NAME')
     except (IOError, Error):
         print('Error with config file...')
@@ -39,25 +39,25 @@ def read_config():
     return pub_address, ack_address, topic, ack_topic, heartbeat_topic, response_topic, sub_address, service_name
 
 
-def schedule_items(jobs, config):
-    sched = ScheduleAuction()
-    sched.initialize_publisher(config[Config.PUB_ADDRESS])
+def setup_scheduler(jobs, config):
+    scheduler = ScheduleAuction()
+    scheduler.initialize_publisher(config[Config.PUB_ADDRESS])
     print('Publisher initialized...')
-    sched.initialize_subscribers(config[Config.ACK_ADDRESS], config[Config.SUB_ADDRESS], config[Config.ACK_TOPIC],
-                                 config[Config.HEARTBEAT_TOPIC], config[Config.HEARTBEAT_RESPONSE],
-                                 config[Config.SERVICE_NAME])
+    scheduler.initialize_subscribers(config[Config.ACK_ADDRESS], config[Config.SUB_ADDRESS], config[Config.ACK_TOPIC],
+                                     config[Config.HEARTBEAT_TOPIC], config[Config.HEARTBEAT_RESPONSE],
+                                     config[Config.SERVICE_NAME])
     print('Subscriber initialized...')
-    sched.initialize_scheduler(jobs, config[Config.TOPIC])
+    scheduler.initialize_scheduler(jobs, config[Config.TOPIC])
 
 
 if __name__ == '__main__':
-    client = initialize_mongo()
-    if None != client:
+    mongo = initialize_mongo()
+    if None != mongo:
         print('Connected to MongoDB...')
-        db = client.AuctionData
-        if None != db:
-            auctions = db.auctions.find()
+        database = mongo.AuctionData
+        if None != database:
+            auctions = database.auctions.find()
             configuration = read_config()
         if None != configuration and None != auctions:
             print('Auction items retrieved...\nConfiguration read...')
-            schedule_items(auctions, configuration)
+            setup_scheduler(auctions, configuration)
--- scheduleauction.py
+++ scheduleauction.py
@@ -16,24 +16,23 @@ context = zmq.Context()
 class ScheduleAuction:
 
     @staticmethod
-    def publish_start_auction_command(item_id, topic):
+    def publish_start_auction_command(auction_item_id, topic):
         if None != publisher:
-            message = topic + ' <id>{id}</id>'.format(id=item_id)
-            publisher.send_string(message)
-            print('PUB: ' + message)
+            start_auction_command = topic + ' <id>{id}</id>'.format(id=auction_item_id)
+            publisher.send_string(start_auction_command)
+            print('PUB: ' + start_auction_command)
 
-    def schedule_jobs(self, sched, auction_items, topic):
-        for item in auction_items:
-            start_time = datetime.strptime(item['start_time'], '%d-%m-%Y %H:%M:%S')
-            if start_time > datetime.now():
-                item_id = item['_id']
-                sched.add_job(self.publish_start_auction_command,
-                              'date', run_date=start_time, kwargs={'item_id': item_id, 'topic': topic})
+    def schedule_auctions(self, sched, auction_items, topic):
+        for auction_item in auction_items:
+            auction_start_time = datetime.strptime(auction_item['start_time'], '%d-%m-%Y %H:%M:%S')
+            if auction_start_time > datetime.now():
+                sched.add_job(self.publish_start_auction_command, 'date', run_date=auction_start_time,
+                              kwargs={'item_id': auction_item['_id'], 'topic': topic})
 
     def initialize_scheduler(self, db_jobs, topic):
         scheduler = BlockingScheduler()
         print('Scheduler initialized...')
-        self.schedule_jobs(scheduler, db_jobs, topic)
+        self.schedule_auctions(scheduler, db_jobs, topic)
         print('Scheduler Running...')
 
         try:
@@ -48,15 +47,15 @@ class ScheduleAuction:
         publisher = context.socket(zmq.PUB)
         publisher.bind(pub_addr)
 
-    def initialize_subscribers(self, ack_adr, sub_adr, ack_topic, heartbeat_topic, response_topic, service_name):
+    def initialize_subscribers(self, ack_addr, sub_addr, ack_topic, heartbeat_topic, response_topic, service_name):
         ack_thread = threading.Thread(target=self.subscribe_to_ack,
-                                      kwargs={'ack_adr': ack_adr, 'ack_topic': str(ack_topic)},
+                                      kwargs={'ack_adr': ack_addr, 'ack_topic': str(ack_topic)},
                                       name='subscribe_to_ack')
         ack_thread.daemon = True
         ack_thread.start()
 
         heartbeat_thread = threading.Thread(target=self.subscribe_to_heartbeat,
-                                            kwargs={'sub_adr': sub_adr, 'heartbeat_topic': str(heartbeat_topic),
+                                            kwargs={'sub_adr': sub_addr, 'heartbeat_topic': str(heartbeat_topic),
                                                     'response_topic': response_topic, 'service_name': service_name},
                                             name='subscribe_to_heartbeat')
         heartbeat_thread.daemon = True
@@ -79,6 +78,6 @@ class ScheduleAuction:
 
         while True:
             print('REC: ' + heartbeat_subscriber.recv().decode())
-            message = response_topic + ' <params>' + service_name + '</params>'
-            publisher.send_string(message)
-            print('PUB: ' + message)
+            heartbeat_response = response_topic + ' <params>' + service_name + '</params>'
+            publisher.send_string(heartbeat_response)
+            print('PUB: ' + heartbeat_response)
